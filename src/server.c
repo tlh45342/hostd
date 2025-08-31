178341827b37d19a5afcd4f7eface4b8b48e637e
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2025-08-31 16:22:52 -0400

    Update server.c

--- src/server.c
+++ src/server.c
@@ -1,4 +1,4 @@
-// server.c - UNIX socket server loop
+// server.c - UNIX and TCP server loop
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,7 +7,11 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <signal.h>
-#include <sys/stat.h> 
+#include <sys/stat.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "hostd.h"
 #include "protocol.h"
@@ -18,7 +22,7 @@ static int create_unix_listener(const char *path) {
     struct sockaddr_un addr;
 
     if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
-        log_msg("socket error: %s\n", strerror(errno));
+        log_msg("socket(AF_UNIX) error: %s\n", strerror(errno));
         return -1;
     }
 
@@ -43,16 +47,54 @@ static int create_unix_listener(const char *path) {
     return fd;
 }
 
+static int create_tcp_listener(const char *bind_host, int port) {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) { log_msg("socket(AF_INET) error: %s\n", strerror(errno)); return -1; }
+
+    int one = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons((uint16_t)port);
+    if (!bind_host || bind_host[0] == 0) {
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    } else {
+        if (inet_pton(AF_INET, bind_host, &addr.sin_addr) != 1) {
+            // try DNS
+            struct hostent *he = gethostbyname(bind_host);
+            if (!he || he->h_addrtype != AF_INET) {
+                log_msg("invalid bind host: %s\n", bind_host ? bind_host : "(null)");
+                close(fd);
+                return -1;
+            }
+            memcpy(&addr.sin_addr, he->h_addr_list[0], sizeof(struct in_addr));
+        }
+    }
+
+    if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        log_msg("bind(tcp) error: %s\n", strerror(errno));
+        close(fd);
+        return -1;
+    }
+    if (listen(fd, 64) < 0) {
+        log_msg("listen(tcp) error: %s\n", strerror(errno));
+        close(fd);
+        return -1;
+    }
+    return fd;
+}
+
 static void handle_client(int cfd) {
     char inbuf[4096];
     char outbuf[4096];
     ssize_t n;
 
-    // Simple line-by-line processing
     while ((n = read(cfd, inbuf, sizeof(inbuf)-1)) > 0) {
         inbuf[n] = 0;
 
-        // process each line separately
+        // process each line
         char *saveptr = NULL;
         char *line = strtok_r(inbuf, "\r\n", &saveptr);
         while (line) {
@@ -74,22 +116,45 @@ int server_run(const char *sock_path) {
     int lfd = create_unix_listener(sock_path);
     if (lfd < 0) return 1;
 
-    log_msg("listening on %s\n", sock_path);
+    log_msg("listening (unix) on %s\n", sock_path);
 
     while (g_running) {
         int cfd = accept(lfd, NULL, NULL);
         if (cfd < 0) {
             if (errno == EINTR) continue;
-            log_msg("accept error: %s\n", strerror(errno));
+            log_msg("accept error (unix): %s\n", strerror(errno));
             break;
         }
-        if (g_verbose) fprintf(stderr, "[hostd] client connected\n");
+        if (g_verbose) fprintf(stderr, "[hostd] client connected (unix)\n");
         handle_client(cfd);
         close(cfd);
-        if (g_verbose) fprintf(stderr, "[hostd] client disconnected\n");
+        if (g_verbose) fprintf(stderr, "[hostd] client disconnected (unix)\n");
     }
 
     close(lfd);
     unlink(sock_path);
+    return 0;
+}
+
+int server_run_tcp(const char *bind_host, int bind_port) {
+    int lfd = create_tcp_listener(bind_host, bind_port);
+    if (lfd < 0) return 1;
+
+    log_msg("listening (tcp) on %s:%d\n", bind_host && bind_host[0]?bind_host:"0.0.0.0", bind_port);
+
+    while (g_running) {
+        int cfd = accept(lfd, NULL, NULL);
+        if (cfd < 0) {
+            if (errno == EINTR) continue;
+            log_msg("accept error (tcp): %s\n", strerror(errno));
+            break;
+        }
+        if (g_verbose) fprintf(stderr, "[hostd] client connected (tcp)\n");
+        handle_client(cfd);
+        close(cfd);
+        if (g_verbose) fprintf(stderr, "[hostd] client disconnected (tcp)\n");
+    }
+
+    close(lfd);
     return 0;
 }
