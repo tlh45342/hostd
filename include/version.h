78ba2f129b6f9acacee7150f4fe4b18ad71d5c91
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2025-08-31 17:36:22 -0400

    Create version.h

---
+++ include/version.h
@@ -0,0 +1,8 @@
+// include/version.h
+#pragma once
+
+// Default if not provided by the build system.
+// You can bump here, or override via -DHOSTD_VERSION="\"x.y.z\""
+#ifndef HOSTD_VERSION
+#define HOSTD_VERSION "0.1.1"
+#endif
